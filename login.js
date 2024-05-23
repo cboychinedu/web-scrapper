5b105b41a77f5dc07d1ffb5928a00e167d3f3ccc
Mbonu Chinedum <cboy.chinedu@gmail.com>
2024-05-23 02:00:20 +0100

    added the .env file

--- login.js
+++ login.js
@@ -17,8 +17,9 @@ const login = async () => {
     await page.click('a[href="/login"]'); 
 
     // typeing the username inside the user text field
+    const username = process.env.username;
     await page.waitForSelector('input[name="text"]')
-    await page.type('input[name="text"]', 'brownmark2179')
+    await page.type('input[name="text"]', username)
 
     // Wait for the button to be rendered on the page
     await page.waitForSelector('#layers > div:nth-child(2) > div > div > div > div > div > div.css-175oi2r.r-1ny4l3l.r-18u37iz.r-1pi2tsx.r-1777fci.r-1xcajam.r-ipm5af.r-g6jmlv.r-1awozwy > div.css-175oi2r.r-1wbh5a2.r-htvplk.r-1udh08x.r-1867qdf.r-kwpbio.r-rsyp9y.r-1pjcn9w.r-1279nm1 > div > div > div.css-175oi2r.r-1ny4l3l.r-6koalj.r-16y2uox.r-kemksi.r-1wbh5a2 > div.css-175oi2r.r-16y2uox.r-1wbh5a2.r-f8sm7e.r-13qz1uu.r-1ye8kvj > div > div > div > button:nth-child(6)');
@@ -28,7 +29,6 @@ const login = async () => {
 
     // Optional: Wait for some result after clicking the button
     // await page.waitForNavigation();
-
 
    // Typing the password
    await page.waitForSelector('input[type="password"]');
